d781b6e89d0e557120e02f6b9b1d01d63657cbce
stlst <stlst@foxmail.com>
2020-09-20 00:03:45 +0800

    lesson 2, enable setState

--- toyReact.ts
+++ toyReact.ts
@@ -1,13 +1,34 @@
+//  使用symbol定义private field
+const RENDER_TO_DOM = Symbol("render to dom");
+
 class ElementWrapper {
   public root: HTMLElement;
   constructor(type) {
     this.root = document.createElement(type);
   }
-  setAttribute(name, value) {
-    this.root.setAttribute(name, value);
+  setAttribute(name: string, value) {
+    if (name.match(/^on([\s\S]+)/)) {
+      // \s\S表示所有非空字符
+      this.root.addEventListener(
+        RegExp.$1.replace(/^[\s\S]/, (c) => c.toLowerCase()),
+        value
+      ); // 兼容驼峰命名
+    } else if (name === "className") {
+      this.root.setAttribute("class", value);
+    } else {
+      this.root.setAttribute(name, value);
+    }
   }
   appendChild(component) {
-    this.root.appendChild(component.root);
+    let range = document.createRange();
+    range.setStart(this.root, this.root.childNodes.length);
+    range.setEnd(this.root, this.root.childNodes.length);
+    component[RENDER_TO_DOM](range);
+  }
+
+  [RENDER_TO_DOM](range: Range) {
+    range.deleteContents();
+    range.insertNode(this.root);
   }
 }
 
@@ -15,6 +36,10 @@ class TextWrapper {
   public root: Text;
   constructor(content: string) {
     this.root = document.createTextNode(content);
+  }
+  [RENDER_TO_DOM](range: Range) {
+    range.deleteContents();
+    range.insertNode(this.root);
   }
 }
 
@@ -25,12 +50,14 @@ export class Component {
   public props;
   public children;
   private _root;
-  //   public render;
+  private _range: Range;
+  public state;
 
   constructor() {
     this.props = Object.create(null);
     this.children = [];
     this._root = null;
+    this._range = (null as unknown) as Range;
   }
   setAttribute(name, value) {
     this.props[name] = value;
@@ -38,11 +65,48 @@ export class Component {
   appendChild(component) {
     this.children.push(component);
   }
-  get root() {
-    if (!this._root) {
-      this._root = this.render().root;
+  // 这里的render to dom 是symble
+  [RENDER_TO_DOM](range: Range) {
+    // 传一个位置过来,把内容插入到range里
+    this._range = range;
+    this.render()[RENDER_TO_DOM](range);
+  }
+
+  rerender() {
+    // 在RENDER_TO_DOM里面会改this._range的值，所以需要先把老的range保存起来
+    const oldRange = this._range;
+    // 需要保证该range不为空，否则下一个会被它吞进去
+    // 因此应该先插入然后再删除
+
+    let range = document.createRange();
+    // range 的起点和终点是一样的
+    range.setStart(oldRange.startContainer, oldRange.startOffset);
+    range.setEnd(oldRange.startContainer, oldRange.startOffset);
+    this[RENDER_TO_DOM](range); // 完成插入
+
+    // 重设oldRange的起点，把oldRange的start挪到插入的内容之后
+    oldRange.setStart(range.endContainer, range.endOffset);
+    oldRange.deleteContents(); // 再把它内容删除
+  }
+
+  setState(newState) {
+    // typeof null 也是个object,所以判断的时候需要注意
+    if (this.state === null || typeof this.state !== "object") {
+      this.state = newState;
+      this.rerender();
+      return;
     }
-    return this._root;
+    const merge = (oldState, newState) => {
+      for (let p in newState) {
+        if (oldState[p] === null || typeof oldState[p] !== "object") {
+          oldState[p] = newState[p]; // 如果OldState的p属性为空，则直接把newState[p]抄写过去
+        } else {
+          merge(oldState[p], newState[p]); // 递归调用merge
+        }
+      }
+    };
+    merge(this.state, newState);
+    this.rerender();
   }
 }
 
@@ -51,7 +115,7 @@ export const createElement = (type, attributes, ...children) => {
   if (typeof type === "string") {
     e = new ElementWrapper(type);
   } else {
-    e = new type(); // ???
+    e = new type(); // 这里的type其实就是Component
     console.log("type", type, e);
   }
   for (let p in attributes) {
@@ -61,6 +125,9 @@ export const createElement = (type, attributes, ...children) => {
     for (let child of children) {
       if (typeof child === "string") {
         child = new TextWrapper(child);
+      }
+      if (child === null) {
+        continue;
       }
       if (typeof child === "object" && child instanceof Array) {
         insertChildren(child);
@@ -76,5 +143,10 @@ export const createElement = (type, attributes, ...children) => {
 (window as any).createElement = createElement;
 
 export const render = (component: Component, parentElement: HTMLElement) => {
-  parentElement.appendChild(component.root);
+  let range = document.createRange();
+  range.setStart(parentElement, 0);
+  // 这里不能用children, 而使用childNodes， 因为这里可能会有文本节点和注释节点
+  range.setEnd(parentElement, parentElement.childNodes.length);
+  range.deleteContents();
+  component[RENDER_TO_DOM](range);
 };
